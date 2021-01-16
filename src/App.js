d99a66386d3f699ee57121233611d5b0fc2c6eb4
miguelzabalaf <zabalafmiguel@hotmail.com>
2021-01-16 09:44:39 -0500

    Display list, play music, skip and back song and visual effects incorporated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react';
+// Components
+import { Song } from './components/Song';
+import { Player } from "./components/player";
+import { Library } from './components/Library';
+import { Nav } from './components/Nav';
+// Styles
+import './styles/app.scss'
+// Data
+import data from './data';
+import { isMobile } from './helpers/isMobile';
 
-function App() {
+const App = () => {
+
+  // Ref
+  const audioRef = useRef(null);
+
+  // State Songs
+  const [songs, setSongs] = useState(data())
+
+  // State Song currently
+  const [currentSong, setCurrentSong] = useState(songs[1])
+
+  //State Playing
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  // State songInfo
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0
+  });
+
+  // State Navbar
+  const [libraryStatus, setLibraryStatus] = useState(false);
+
+  const timeUpdateHandler = (event) => {
+    const currentAudioTime = event.target.currentTime;
+    const currentAudioDuration = event.target.duration;
+    setSongInfo({
+      ...songInfo,
+      currentTime: currentAudioTime,
+      duration: currentAudioDuration
+    })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${libraryStatus & !isMobile() ? 'library-active' : ''}`}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+        songs={songs}
+      />
+      <Library
+        libraryStatus={libraryStatus}
+        audioRef={audioRef}
+        songs={songs}
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        setLibraryStatus={setLibraryStatus}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 }
